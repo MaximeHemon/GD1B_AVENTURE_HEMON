3c88fb744a0ccb9e63a9c94873cca039c0482b34
MaximeHemon <74669577+MaximeHemon@users.noreply.github.com>
2021-05-05 13:42:55 +0200

    Add files via upload

--- Scene1.js
+++ Scene1.js
@@ -8,21 +8,13 @@ class Scene1 extends Phaser.Scene{
     init(data){
     }
     preload(){   
-        this.load.image('background','assets/scene_1'.png');
-        this.load.tilemapTiledJSON('map_1_placeholder', 'map_1_placeholder.json');
-        this.load.image('player', 'assets/player.png');
-    }
+
     create(){
-        const map = this.make.tilemap({key: 'map_1_placeholder'});
-        const tileset = map.addTilesetImage('tileset_placeholder', 'tiles');
-        const terrain = map.createStaticLayer('terrain', tileset, 0, 0);
-        const bloquant = map.createStaticLayer('bloquant', tileset, 0, 0);
-        const zone = map.createStaticLayer('zone', tileset, 0, 0);
 
         bloquant.setCollisionByExclusion(-1, true);
         zone.setCollisionByExclusion(-1, true);
 
-        player = this.physics.add.sprite(300, 300, 'player');
+        player = this.physics.add.sprite(XXX, XXX, 'player');
 
         this.physics.add.collider(player, bloquant);
         this.physics.add.overlap(player, zone, changementZone, null, this);
